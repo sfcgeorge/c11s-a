94911367e67890285633cfb530751f4b541608cc
Simon George <simon@sfcgeorge.co.uk>
2015-08-24 14:45:13 +0100

    More settings work

--- src/time_complication.c
+++ src/time_complication.c
@@ -7,6 +7,8 @@ void time_complication_update(void) {
   struct tm *tm = localtime(&now);
   snprintf(time_complication_year, 5, "%d", 1900 + tm->tm_year);
   snprintf(time_complication_month, 3, "%d", tm->tm_mon + 1);
+  char month_names[12][4] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+  strcpy(time_complication_month_name, month_names[tm->tm_mon], 4);
 
   time_complication_hour_angle = (TRIG_MAX_ANGLE * (((tm->tm_hour % 12) * 6) + (tm->tm_min / 10))) / (12 * 6);
   time_complication_minute_angle = TRIG_MAX_ANGLE * tm->tm_min / 60;
